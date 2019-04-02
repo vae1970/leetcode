7169ba866aed1c8cb19b331792f50953590dc25e
dongzhou.gu <496075305@qq.com>
2019-04-02 18:53:23 +0800

    perf: Longest Palindromic Substring

--- src/main/java/com/vae1970/leetcode/solution/LongestPalindromicSubstring.java
+++ src/main/java/com/vae1970/leetcode/solution/LongestPalindromicSubstring.java
@@ -9,26 +9,28 @@ public class LongestPalindromicSubstring {
 
     public static String longestPalindrome(String s) {
         int tempLeft, tempRight, pointerLeft = 0, pointerRight = 0;
-        int length = s.length();
+        int length = s.length(), max = length - 1;
         if (length > 1) {
             if (s.charAt(0) == s.charAt(1)) {
                 pointerRight = 1;
             }
             if (length > 2) {
-                for (int i = 1, im = length - 1; i < im; i++) {
-                    boolean rightEqual = s.charAt(i) == s.charAt(i + 1);
-                    boolean leftAndRightEqual = s.charAt(i - 1) == s.charAt(i + 1);
+                for (int i = 1; i < max; i++) {
+                    int previous = i - 1;
+                    int next = i + 1;
+                    boolean rightEqual = s.charAt(i) == s.charAt(next);
+                    boolean leftAndRightEqual = s.charAt(previous) == s.charAt(next);
                     while (rightEqual || leftAndRightEqual) {
                         if (rightEqual) {
                             tempLeft = i;
-                            tempRight = i + 1;
+                            tempRight = next;
                             rightEqual = false;
                         } else {
-                            tempLeft = i - 1;
-                            tempRight = i + 1;
+                            tempLeft = previous;
+                            tempRight = next;
                             leftAndRightEqual = false;
                         }
-                        for (; tempLeft > 0 && tempRight < length - 1; ) {
+                        for (; tempLeft > 0 && tempRight < max; ) {
                             tempLeft--;
                             tempRight++;
                             if (s.charAt(tempLeft) != s.charAt(tempRight)) {
