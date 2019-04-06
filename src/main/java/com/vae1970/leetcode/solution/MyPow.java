f332a0601c0988ea8276fd62b11f5cc3b39a8297
vae1970 <496075305@qq.com>
2019-04-06 23:55:12 +0800

    pref: myPow

--- src/main/java/com/vae1970/leetcode/solution/MyPow.java
+++ src/main/java/com/vae1970/leetcode/solution/MyPow.java
@@ -16,16 +16,26 @@ public class MyPow {
         System.out.println(6&1);
     }
 
-    public static double myPow(double x, int n) {
-        if (n == 0) {
-            return 1;
-        } else if (n > 0) {
-            return myPowPositive(x, n);
-        } else {
-            return 1 / myPowPositive(x, -n);
-        }
-    }
+//    public static double myPow(double x, int n) {
+//        if (n == 0) {
+//            return 1;
+//        } else if (n > 0) {
+//            return myPowPositive(x, n);
+//        } else {
+//            return 1 / myPowPositive(x, -n);
+//        }
+//    }
 
+    public static double myPow(double x, int n) {
+        double res = 1.0;
+        for(int i = n; i != 0; i /= 2){
+            if(i % 2 != 0){
+                res *= x;
+            }
+            x *= x;
+        }
+        return  n < 0 ? 1 / res : res;
+    }
 
     public static double myPowPositive(double x, int n) {
         double doubleTemp = x * x;
