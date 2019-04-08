00f844974152324fd35b2b0a6c16a6a144347a3d
vae1970 <496075305@qq.com>
2019-04-08 22:51:02 +0800

    refactor: myPow

--- src/main/java/com/vae1970/leetcode/solution/MyPow.java
+++ src/main/java/com/vae1970/leetcode/solution/MyPow.java
@@ -13,20 +13,38 @@ public class MyPow {
         System.out.println(myPow(2.00000, -2));
         System.out.println(myPow(1.00001, 123456));
         System.out.println(myPow(0.00001, 2147483647));
+        System.out.println(myPow(0.44528, 0));
+        System.out.println(myPow(1.00000, -2147483648));
         System.out.println(6 & 1);
         System.out.println(-2 / 2);
     }
 
+    /**
+     * 值   临时值   递归     递归
+     * 39   1       38+1    2n+1
+     * 19   2       18+1    2n+1
+     * 9    4       8+1     2n+1
+     * 4    8+1     4       2n
+     * 2    18+1    2       2n
+     * 1    38+1    0+1     2n+1
+     * 0    0       0       0
+     *
+     * @param x x
+     * @param n n
+     * @return pow
+     */
     public static double myPow(double x, int n) {
-        double pow = 1;
-        boolean positive = n > 0;
-        for (; n != 0; n /= 2) {
-            if (n % 2 != 0) {
-                pow *= x;
-            }
-            x *= x;
+        return n > 0 ? pow(x, n) : 1 / pow(x, n);
+    }
+
+    public static double pow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            double pow = pow(x, n / 2);
+            pow *= pow;
+            return n % 2 == 0 ? pow : pow * x;
         }
-        return positive ? pow : 1 / pow;
     }
 
 }
