06138c348b6e693802369d8ef4589845c1e1b5bc
vae1970 <496075305@qq.com>
2019-04-12 23:56:07 +0800

    fix: Remove Duplicates from Sorted Array

--- src/main/java/com/vae1970/leetcode/solution/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/vae1970/leetcode/solution/RemoveDuplicatesFromSortedArray.java
@@ -1,5 +1,7 @@
 package com.vae1970.leetcode.solution;
 
+import java.util.Arrays;
+
 /**
  * Remove Duplicates from Sorted Array
  *
@@ -8,18 +10,39 @@ package com.vae1970.leetcode.solution;
 public class RemoveDuplicatesFromSortedArray {
 
     public static void main(String[] args) {
-
+        System.out.println(removeDuplicates(new int[]{1, 1, 2}));
+        System.out.println(removeDuplicates(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}));
     }
 
-    public int removeDuplicates(int[] nums) {
+    public static int removeDuplicates(int[] nums) {
+        int j;
         for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
-                nums[i + 1] = 0;
-
-
+            j = i + 1;
+            if (nums[i] == nums[j]) {
+                nums[j] = 0;
+                for (j++; j < nums.length; j++) {
+                    if (nums[i] == nums[j]) {
+                        nums[j] = 0;
+                    } else {
+                        break;
+                    }
+                }
                 i++;
             }
         }
+        System.out.println(Arrays.toString(nums));
+        j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > -1) {
+                nums[j] = nums[i];
+                if (i != j) {
+                    nums[i] = 0;
+                }
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+        return j;
     }
 
 }
