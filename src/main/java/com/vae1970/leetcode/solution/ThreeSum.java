9b7bc64330eefa5a4805e5f99f9f7320bf3cc18e
vae1970 <496075305@qq.com>
2019-04-14 23:52:10 +0800

    docs: 3Sum

--- src/main/java/com/vae1970/leetcode/solution/ThreeSum.java
+++ src/main/java/com/vae1970/leetcode/solution/ThreeSum.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 3Sum
+ *
+ * @author vae
+ */
 public class ThreeSum {
 
     public static void main(String[] args) {
