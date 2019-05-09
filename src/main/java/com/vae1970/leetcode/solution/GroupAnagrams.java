9505fbbff38f70fadea277f8ea49b3a7270dfe1c
vae1970 <496075305@qq.com>
2019-05-09 23:55:53 +0800

    fix: Group Anagrams

--- src/main/java/com/vae1970/leetcode/solution/GroupAnagrams.java
+++ src/main/java/com/vae1970/leetcode/solution/GroupAnagrams.java
@@ -1,8 +1,6 @@
 package com.vae1970.leetcode.solution;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -12,37 +10,114 @@ import java.util.List;
  */
 public class GroupAnagrams {
     public static void main(String[] args) {
-        String[] s = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        String[] s = new String[]{"eat", "tea", "tan", "ate", "nat", "bat", "", ""};
+        String[] s1 = new String[]{"", "b"};
+        String[] s2 = new String[]{"tea", "", "eat", "", "tea", ""};
+        String[] s3 = new String[]{"cab", "tin", "pew", "duh", "may", "ill", "buy", "bar", "max", "doc"};
+        String[] s4 = new String[]{"duh", "ill"};
         System.out.println(groupAnagrams(s));
+        System.out.println(groupAnagrams(s1));
+        System.out.println(groupAnagrams(s2));
+        System.out.println(groupAnagrams(s3));
+        System.out.println(groupAnagrams(s4));
+        System.out.println(groupAnagrams(new String[]{""}));
     }
 
     public static List<List<String>> groupAnagrams(String[] strs) {
-
+        List<String> hashList = new ArrayList<>();
         List<List<String>> result = new ArrayList<>();
-        boolean empty = false;
-        List<Long> hashArray = new ArrayList<>();
         for (int i = 0; i < strs.length; i++) {
-            Long hash = hash(strs[i]);
-            if (hash == null) {
-                empty = true;
-            } else {
-
-            }
-
+            String hash = hash(strs[i]);
+            int index = getIndex(hash, hashList);
+            List<String> list = get(result, index);
+            list.add(strs[i]);
         }
-
-
+        return result;
     }
 
-    public static Long hash(String s) {
-        Long hash = null;
-        if (s != null) {
-            hash = 0L;
-            for (int j = 0; j < s.length(); j++) {
-                hash += s.charAt(j);
+    public static String hash(String s) {
+        if (s == null) {
+            return null;
+        } else {
+            char[] chars = s.toCharArray();
+            sort(chars, 0, chars.length - 1);
+            return new String(chars);
+        }
+    }
+
+    public static int getIndex(String hash, List<String> hashList) {
+        int index = hashList.indexOf(hash);
+        if (index < 0) {
+            hashList.add(hash);
+            return hashList.size() - 1;
+        } else {
+            return index;
+        }
+    }
+
+    public static List<String> get(List<List<String>> list, int index) {
+        List<String> result;
+        if (list.size() > index) {
+            result = list.get(index);
+        } else {
+            result = new ArrayList<>();
+            list.add(result);
+        }
+        return result;
+    }
+
+    public static void sort(char[] array, int start, int end) {
+        int i = start;
+        int j = end;
+        boolean out = false;
+        if (start < end) {
+            while (true) {
+                if (compare(array[j], array[start]) < 0) {
+                    for (; i < j; i++) {
+                        if (compare(array[i], array[start]) > 0) {
+                            swap(array, i, j);
+                            out = true;
+                            break;
+                        }
+                    }
+                }
+                if (out) {
+                    out = false;
+                } else {
+                    if (i < j) {
+                        j--;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            if (start != j) {
+                swap(array, start, j);
+            }
+            if (i > start) {
+                i--;
+            }
+            if (j < end) {
+                j++;
+            }
+            if (start != i) {
+                sort(array, start, i);
+            }
+            if (j != end) {
+                sort(array, j, end);
             }
         }
-        return hash;
     }
+
+    public static void swap(char[] array, int start, int end) {
+        char temp = array[start];
+        array[start] = array[end];
+        array[end] = temp;
+    }
+
+    public static int compare(int a, int b) {
+        return a - b;
+    }
+
 
 }
