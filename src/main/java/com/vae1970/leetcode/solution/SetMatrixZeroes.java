7d32ede65156d969123ce06a9a32db54bc7ff71c
gudz <496075305@qq.com>
2019-08-20 14:49:06 +0800

    perf: Set Matrix Zeroes

--- src/main/java/com/vae1970/leetcode/solution/SetMatrixZeroes.java
+++ src/main/java/com/vae1970/leetcode/solution/SetMatrixZeroes.java
@@ -1,9 +1,5 @@
 package com.vae1970.leetcode.solution;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-
 /**
  * @author dongzhou.gu
  * @date 2019/8/20
@@ -15,35 +11,40 @@ public class SetMatrixZeroes {
         SetMatrixZeroes setMatrixZeroes = new SetMatrixZeroes();
         int[][] a = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
         int[][] b = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
-        long start = System.currentTimeMillis();
+        long start = System.nanoTime();
         setMatrixZeroes.setZeroes(b);
-        long end = System.currentTimeMillis();
-        System.out.println("ms: " + (end - start));
+        long end = System.nanoTime();
+        System.out.println("ms: " + (float) (end - start) / 1000000L);
     }
 
     public void setZeroes(int[][] matrix) {
-        Set<Integer> row = new HashSet<>(matrix.length);
-        Set<Integer> column = new HashSet<>(matrix[0].length);
+        //  标记需要重置的行与列
+        boolean[] row = new boolean[matrix.length];
+        boolean[] column = new boolean[matrix[0].length];
+        //  遍历二维数组，标记为0的行与列
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] == 0) {
-                    row.add(i);
-                    column.add(j);
+                    row[i] = true;
+                    column[j] = true;
                 }
             }
         }
-        Iterator<Integer> rowIterator = row.iterator();
-        while (rowIterator.hasNext()) {
-            Integer i = rowIterator.next();
-            for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = 0;
+        int i, j;
+        //  遍历重置的行
+        for (i = 0; i < row.length; i++) {
+            if (row[i]) {
+                for (j = 0; j < matrix[i].length; j++) {
+                    matrix[i][j] = 0;
+                }
             }
         }
-        Iterator<Integer> columnIterator = column.iterator();
-        while (columnIterator.hasNext()) {
-            Integer j = columnIterator.next();
-            for (int i = 0; i < matrix.length; i++) {
-                matrix[i][j] = 0;
+        //  遍历重置的列
+        for (j = 0; j < column.length; j++) {
+            if (column[j]) {
+                for (i = 0; i < matrix.length; i++) {
+                    matrix[i][j] = 0;
+                }
             }
         }
     }
